b8a024a69b72a058854c0ff3bae9cc4a73dfcd9a
bhaskardevalanka <122113889+bhaskardevalanka@users.noreply.github.com>
2024-01-04 01:00:27 +0530

    Update TestController.java

--- src/main/java/io/getarrays/cicdpipeline/web/TestController.java
+++ src/main/java/io/getarrays/cicdpipeline/web/TestController.java
@@ -22,11 +22,11 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<Map<String, String>> healthcheck() {
-        return ResponseEntity.ok().body(Map.of("Application Up and Running"));
+        return ResponseEntity.ok().body(Map.of("Status", "Up"));
     }
 
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> test() {
-        return ResponseEntity.ok().body(Map.of("Testing Completed"));
+        return ResponseEntity.ok().body(Map.of("Testing", "Test Data"));
     }
 }
